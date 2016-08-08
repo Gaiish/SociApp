2559bdbff1df675755e918590dc961786eba24e0
Super User <superuser@localhost.localdomain>
2016-08-08 20:01:36 +0300

    updated the dashboard

--- SociApp/settings.py
+++ SociApp/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
+LOGIN_REDIRECT = reverse_lazy('dsshboard')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 #from django.contrib.auth import views
+#import django
 
 urlpatterns = [
     #url(r'^login/$', views.user_login, name="login"),
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name="logout_then_login"),
+    url(r'^$', views.dashboard, name="dashboard"),
 ]
